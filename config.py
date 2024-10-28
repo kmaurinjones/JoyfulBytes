12f99f5822753cc67c4da5e22cbc1a5169eb1663
kmaurinjones <kmaurinjones@gmail.com>
2024-10-28 08:37:46 -0400

    Scheduled update: 20242810-083746

--- config.py
+++ config.py
@@ -1,13 +1,13 @@
 CONFIG = {
     "search_queries": [
         "Joyful news today",
-        "Incredible kindness today",
-        "Beautiful moments in the news",
         "Heartwarming acts today",
-        "Smiles and happiness today",
         "Feel-good stories today",
-        "Unbelievable generosity today",
-        "Love and hope in the news",
-        "Stories that spark joy today"
+        "Positive stories from north america now",
+        "Uplifting news south america yesterday",
+        "Good news happening in europe now",
+        "Heartwarming stories asia today",
+        "Inspiring moments africa today",
+        "Happy news from australia recently"
     ]
 }
